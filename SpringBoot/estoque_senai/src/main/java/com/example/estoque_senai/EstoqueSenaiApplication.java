9da93a40251c681039a08422fed030f9df80f013
lpmalosso <lpmalosso@gmail.com>
2026-03-12 08:21:41 -0300

    12/03 - Criação do Projeto Estoque_Senai e preparação de pastas para desenvolvimento

---
+++ SpringBoot/estoque_senai/src/main/java/com/example/estoque_senai/EstoqueSenaiApplication.java
@@ -0,0 +1,13 @@
+package com.example.estoque_senai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstoqueSenaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstoqueSenaiApplication.class, args);
+	}
+
+}
